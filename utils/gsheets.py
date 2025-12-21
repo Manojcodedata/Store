1ee0746d602959df1896ee445440f27eafec6923
Manoj <saimanojpacha@gmail.com>
2025-12-21 23:41:47 +0530

    credential removal for deployment

--- utils/gsheets.py
+++ utils/gsheets.py
@@ -2,6 +2,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from config.settings import SPREADSHEETS
 import os
+import streamlit as st
 
 def get_worksheet(spreadsheet_name, worksheet_name):
     scopes = [
@@ -9,14 +10,9 @@ def get_worksheet(spreadsheet_name, worksheet_name):
         "https://www.googleapis.com/auth/drive"
     ]
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    creds_path = os.path.join(BASE_DIR, "credentials", "credentials.json")
-
-    creds = Credentials.from_service_account_file(
-        creds_path,
-        scopes=scopes
-    )
+    creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scopes)
 
     client = gspread.authorize(creds)
     spreadsheet = client.open(spreadsheet_name)
