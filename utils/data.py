6a20aea8997f87c78d45922e48692a158ef1b7a4
Manoj <saimanojpacha@gmail.com>
2025-12-21 22:33:10 +0530

    Excel file add to new data folder

--- utils/data.py
+++ utils/data.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import os
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_PATH = os.path.join(BASE_DIR, "data", "materials.xlsx")
 def db():
-    base_dir = os.path.dirname(os.path.dirname(__file__))
 
-    file_path = os.path.join(base_dir, "materials.xlsx")
-
-    df = pd.read_excel(file_path)
+    df = pd.read_excel(DATA_PATH)
 
     items = list(df['Item'].values)
     return items
