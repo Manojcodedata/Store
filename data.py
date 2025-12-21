f6343e592d59a303b47a33af498afd71e078754b
Sai Manoj Pacha <147235889+Manojcodedata@users.noreply.github.com>
2025-12-21 23:42:53 +0530

    Delete data.py

--- data.py
+++
@@ -1,9 +0,0 @@
-def db():
-    departments = ["Main Kitchen","Juice","Grinding","Dosa","Parotta","Poori_Snacks","First_Floor","Coffee","House Keeping","Parcel"]
-    import pandas as pd
-
-    df = pd.read_excel("Material Master.xlsx")
-
-    items = list(df['Item'].values)
-
-    return departments, items
